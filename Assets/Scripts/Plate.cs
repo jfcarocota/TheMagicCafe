c88d746e1730f83132dbb43181014acb150182dc
jfcarocota <jfcarocota@gmail.com>
2019-04-14 05:17:58 -0700

    bring animation and plates fbx added

---
+++ Assets/Scripts/Plate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
